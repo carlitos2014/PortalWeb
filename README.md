f3d9a7bda6feb1c31b54cdb0ee710f8f1d3eadda
leobetancourt92 <betancourt92@hotmail.com>
2015-01-11 12:21:41 -0500

    11-01-2015

--- README.md
+++ README.md
@@ -4,10 +4,4 @@ PortalWeb
 proyecto formativo del Sena ,creado a partir de Sohoframework(derivacion de Symphony)
 
 -la tabla usuario cuenta con tres funciones del CRUD (Create,Read,Update) falta la accion de eliminar(delete)
-<<<<<<< HEAD
 -las tablas usuariocredencial,credencial,recordarMe y bitacora cuentan con las grillas de visualizacion.
--98% de los objetivos de el CRUD de las 5 tablas,se podria decir que cumple con todos los objetivos
--solo falta la funcionalidad de los selects de usuario-credencial
-=======
--las tablas usuariocredencial,credencial,recordarMe y bitacora cuentan con las grillas de visualizacion.
->>>>>>> c3a909c6d4ce4ccf630c4927984f85e5be0a48b9
--- view/bitacora/insertTemplate.html.php
+++ view/bitacora/insertTemplate.html.php
@@ -1,6 +1,5 @@
 <?php use mvc\routing\routingClass as routing ?>
 <?php use mvc\i18n\i18nClass as i18n ?>
-<<<<<<< HEAD
 <?php use mvc\view\viewClass as view ?>
 <div class="jumbotron">
             <h1>Creacion de Usuario</h1>
@@ -8,12 +7,3 @@
 
         </div>
 <?php view::includePartial('usuario/formUser') ?>
-=======
-<form method="post" action="<?php echo routing::getInstance()->getUrlWeb('default', 'create') ?>">
-  <?php echo i18n::__('user') ?>: <input type="text" name="<?php echo usuarioTableClass::getNameField(usuarioTableClass::USUARIO, true) ?>">
-  <br>
-  <?php echo i18n::__('pass') ?>: <input type="password" name="<?php echo usuarioTableClass::getNameField(usuarioTableClass::PASSWORD, true) ?>">
-  <br>
-  <input type="submit" value="<?php echo i18n::__('register') ?>">
-</form>
->>>>>>> c3a909c6d4ce4ccf630c4927984f85e5be0a48b9
