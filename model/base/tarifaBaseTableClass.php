509c6f130623a0e13be56440cb32ec4135711dfe
leonardo <leonardo@leonardo-HP-G42-Notebook-PC>
2015-02-19 10:04:46 -0500

    completando base y tableBase

---
+++ model/base/tarifaBaseTableClass.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+use mvc\model\table\tableBaseClass;
+/**
+ * Description of tarifaBaseTableClass
+ *
+ * @author leonardo
+ */
+class tarifaBaseTableClass extends tableBaseClass {
+  //put your code here
+}
