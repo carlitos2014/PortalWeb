b05044f200fe91319c68d3947b36006bc395d026
carlitos <carlitos@carlitos>
2015-03-19 00:20:51 -0500

    EVENTO
    OBJETO Y SELECT DE EVENTO_PATROCINADOR

--- controller/evento/insertActionClass.php
+++ controller/evento/insertActionClass.php
@@ -15,17 +15,26 @@ use mvc\i18n\i18nClass as i18n;
  */
 class insertActionClass extends controllerClass implements controllerActionInterface {
 
-  public function execute() {
-    try {
-      //$this->mensaje = 'HOLA MUNDO';
-      $this->defineView('insert', 'evento', session::getInstance()->getFormatOutput());
-    } catch (PDOException $exc) {
-      echo $exc->getMessage();
-      echo '<br>';
-      echo '<pre>';
-      print_r($exc->getTrace());
-      echo '</pre>';
+    public function execute() {
+        try {
+            //$this->mensaje = 'HOLA MUNDO';
+            //CREACCION DE OBJETOS
+            $fields=array(patrocinadorTableClass::ID, patrocinadorTableClass::NOMBRE);
+            $this->objPatrocinador = patrocinadorTableClass::getAll($fields);
+            $fields1=array(categoriaTableClass::ID, categoriaTableClass::NOMBRE);
+            $this->objCategoria=  categoriaTableClass::getAll($fields1);
+            $fields2=  array( usuarioTableClass::ID, usuarioTableClass::USER);
+            $this-> objUsuario=  usuarioTableClass::getAll($fields2);
+            
+            
+            $this->defineView('insert', 'evento', session::getInstance()->getFormatOutput());
+        } catch (PDOException $exc) {
+            echo $exc->getMessage();
+            echo '<br>';
+            echo '<pre>';
+            print_r($exc->getTrace());
+            echo '</pre>';
+        }
     }
-  }
 
 }
